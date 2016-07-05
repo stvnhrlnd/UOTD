2b447c8f0a937d90fe14086d4667cecd031c644a
Steven <steven_harland@hotmail.co.uk>
2016-07-05 23:03:37 +0100

    Remove unneeded assets and add home page placeholder

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -10,28 +10,4 @@ from FlaskWebProject import app
 @app.route('/home')
 def home():
     """Renders the home page."""
-    return render_template(
-        'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-    )
-
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
-
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
+    return render_template('index.html')
