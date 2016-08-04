f8ae8d496b2ede975911a4ae8a7ff8142ae2b0e1
Steven <steven_harland@hotmail.co.uk>
2016-08-04 20:38:16 +0100

    Add copy to clipboard button

---
+++ FlaskWebProject/static/scripts/copy-button.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    var copyButton = new Clipboard('#copy-button');
+    var copyButtonLabel = document.getElementById('copy-button-label');
+
+    copyButton.on('success', function (e) {
+        var innerHTML = copyButtonLabel.innerHTML;
+        copyButtonLabel.innerHTML = 'Copied';
+
+        setTimeout(function () {
+            copyButtonLabel.innerHTML = innerHTML;
+        }, 2000);
+    });
+}());
