7feb77516e4ca89616b4362a16e4366a1d56aca2
Steven <steven_harland@hotmail.co.uk>
2016-08-14 09:36:12 +0100

    Implement archive page

--- FlaskWebProject/azuretablestorage.py
+++ FlaskWebProject/azuretablestorage.py
@@ -13,34 +13,27 @@ class Repository(object):
         self.partition_key_format = "%Y%m"
         self.row_key_format = "%d"
 
-    def get_uotd(self, date_obj=None):
-        if date_obj is None:
-            partition_key = date.today().strftime(self.partition_key_format)
-            row_key = date.today().strftime(self.row_key_format)
+    def get_uotd(self):
+        partition_key = date.today().strftime(self.partition_key_format)
+        row_key = date.today().strftime(self.row_key_format)
 
-            try:
-                uotd_entity = self.service.get_entity(self.uotd_table,
-                                                      partition_key,
-                                                      row_key)
-                uuid = uotd_entity.uuid
-            except AzureMissingResourceHttpError:
-                uuid = str(uuid1())
-                uotd_entity = {
-                    "PartitionKey": partition_key,
-                    "RowKey": row_key,
-                    "uuid": uuid
-                }
-                self.service.insert_entity(self.uotd_table, uotd_entity)
-        else:
-            partition_key = date_obj.strftime(self.partition_key_format)
-            row_key = date_obj.strftime(self.row_key_format)
-
-            try:
-                uotd_entity = self.service.get_entity(self.uotd_table,
-                                                      partition_key,
-                                                      row_key)
-                uuid = uotd_entity.uuid
-            except AzureMissingResourceHttpError:
-                uuid = None
+        try:
+            uotd_entity = self.service.get_entity(self.uotd_table,
+                                                  partition_key,
+                                                  row_key)
+            uuid = uotd_entity.uuid
+        except AzureMissingResourceHttpError:
+            uuid = str(uuid1())
+            uotd_entity = {
+                "PartitionKey": partition_key,
+                "RowKey": row_key,
+                "uuid": uuid
+            }
+            self.service.insert_entity(self.uotd_table, uotd_entity)
 
         return uuid
+
+    def get_uuids(self, partition_key):
+        filter = "PartitionKey eq '{0}'".format(partition_key)
+        entities = self.service.query_entities(self.uotd_table, filter=filter)
+        return entities
--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -1,5 +1,5 @@
-from datetime import date
-from flask import render_template
+from datetime import date, datetime
+from flask import abort, render_template, url_for
 from FlaskWebProject import app, azuretablestorage, settings
 
 repository = azuretablestorage.Repository(settings.REPOSITORY_SETTINGS)
@@ -12,6 +12,43 @@ def home():
     return render_template("index.html", uuid=uuid, today=today)
 
 
-@app.route("/archive")
-def archive():
-    return render_template("archive.html")
+@app.route("/archive", defaults={'partition_key': None})
+@app.route("/archive/<partition_key>")
+def archive(partition_key):
+    if partition_key is None:
+        date_obj = date.today()
+        partition_key = date_obj.strftime("%Y%m")
+    else:
+        try:
+            date_obj = datetime.strptime(partition_key, "%Y%m")
+        except ValueError:
+            abort(404)
+
+    uuids = repository.get_uuids(partition_key)
+    display_month = date_obj.strftime("%B %Y")
+
+    if date_obj.month == 12:
+        next_month = date(date_obj.year + 1, 1, date_obj.day)
+    else:
+        next_month = date(date_obj.year, date_obj.month + 1, date_obj.day)
+
+    if date_obj.month == 1:
+        prev_month = date(date_obj.year - 1, 12, date_obj.day)
+    else:
+        prev_month = date(date_obj.year, date_obj.month - 1, date_obj.day)
+
+    next_url = url_for('archive', partition_key=next_month.strftime("%Y%m"))
+    prev_url = url_for('archive', partition_key=prev_month.strftime("%Y%m"))
+
+    return render_template("archive.html",
+                           uuids=uuids,
+                           display_month=display_month,
+                           next_url=next_url,
+                           prev_url=prev_url)
+
+
+@app.template_filter("display_day")
+def display_day(uuid):
+    date_str = "{0}{1}".format(uuid.PartitionKey, uuid.RowKey)
+    date_obj = datetime.strptime(date_str, "%Y%m%d")
+    return date_obj.strftime("%A, %d")
